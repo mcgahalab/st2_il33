aded0913e4202d76e6ba6c253c46ba89c9ec3a1b
Rene Quevedo <>
2022-05-09 15:40:43 -0400

    Added an AUCplot for a single delta-sample, one without an interaction term

--- functions/msm_ssm_functions.R
+++ functions/msm_ssm_functions.R
@@ -37,7 +37,7 @@ calcMeanAUC <- function(AUC, cellAnnotation){
 # and the delta-values for both the Interaction-term and the Cis_TDLN-LN 
 # condition, given a matrix of both.
 aucPlot <- function(int_i, grp2_i, grp1_i=NULL, regulons=NULL,  new_ids=NULL,
-                    celltype=NULL, auc_pattern="CIS|PBS|LN|TDLN",  
+                    celltype=NULL, auc_pattern="CIS|PBS|LN|TDLN|DAB|DMSO",  
                     delta_grp1="delta.tdln_ln", delta_grp2='delta.auc'){
   delta_col <- c('delta_int'='#f03b20',
                  'delta_cis'='#636363')
@@ -98,6 +98,61 @@ aucPlot <- function(int_i, grp2_i, grp1_i=NULL, regulons=NULL,  new_ids=NULL,
             align = "h", axis='bt', rel_widths = c(3,1,1))
 }
 
+aucPlotSingle <- function(int_i, regulons=NULL,  new_ids=NULL,
+                          celltype=NULL, auc_pattern="CIS|PBS|LN|TDLN|DAB|DMSO",  
+                          delta_var="delta.auc"){
+  delta_col <- c('delta_int'='#f03b20',
+                 'delta_cis'='#636363')
+  
+  intx <- reshape2::melt(int_i) %>%
+    mutate(Regulon=as.character(Regulon))
+  
+  if(is.null(regulons)){
+    # regulons <- .getRegulons(grp1x, grp2x)
+  }
+  
+  gg_auc <- intx %>%
+    filter(grepl(auc_pattern, variable)) %>%
+    filter(Regulon %in% regulons) %>%
+    mutate(Regulon=factor(as.character(Regulon), levels=regulons)) %>%
+    ggplot(., aes(x=value, y=Regulon, fill=variable)) +
+      geom_bar(position='dodge', stat='identity') +
+      theme_classic() +
+      xlim(0, 1) + xlab("AUC") +
+      scale_fill_manual(values = c("#2c7fb8", "#41b6c4"), 
+                        name = "") +
+      ggtitle(celltype) +
+      theme(legend.position='bottom',
+            legend.justification = "right") 
+  
+  .pltDelta <- function(x, delta_var, new_id=' (Cis)', xlim=0.5, col='darkgrey'){
+    ylab <- delta_var %>%
+      gsub("delta", "Delta", .) %>%
+      gsub("\\.auc", paste0(" ", new_id), .) %>%
+      gsub("\\.tdln_ln", " (TDLN-LN)", .)
+    x %>%
+      filter(grepl(delta_var, variable)) %>%
+      filter(Regulon %in% regulons) %>%
+      mutate(Regulon=factor(as.character(Regulon), levels=regulons)) %>%
+      ggplot(., aes(x=value, y=Regulon)) +
+      geom_bar(position='dodge', stat='identity', fill=col) +
+      theme_classic() + 
+      xlim((-1 * xlim), xlim) + xlab(ylab) + 
+      geom_vline(xintercept=0) +
+      theme(axis.text.y=element_blank(),
+            axis.line.y=element_blank(),
+            axis.title.y=element_blank(),
+            axis.title.x = element_text(size=8),
+            axis.text.x=element_text(angle=90))
+  }
+  
+  gg_intx_delta <- .pltDelta(intx, delta_var=delta_var, new_id=new_ids[1], col=delta_col[1], xlim=0.5)
+
+  
+  plot_grid(gg_auc, gg_intx_delta, nrow=1,
+            align = "h", axis='bt', rel_widths = c(3,1))
+}
+
 # Stand alone function to create a demo plot fo the barplots and how to 
 # interpret the results
 demoAucPlot <- function(){
@@ -153,16 +208,24 @@ demoAucPlot <- function(){
 # delta, combine the values into their mean-absolute-value per regulon, and 
 # then order in ascending order
 .getRegulons <- function(intx, cisx,  
-                         delta_int="delta.tdln_ln", delta_cis='delta.auc'){
+                         delta_int="delta.tdln_ln", delta_cis='delta.auc',
+                         return_df=FALSE){
   delta_comb <- intx %>%
-    filter(grepl(delta_int, variable)) %>%
-    select(Regulon, value) %>%
-    full_join(cisx %>%
+    filter(grepl(delta_int, variable)) %>%  # select only delta-values between condition 1-2
+    select(Regulon, value) %>%              # select only the Regulon + delta-values
+    full_join(cisx %>%                      # full_join with intx with cisx based on Regulon
                 filter(grepl(delta_cis, variable)) %>%
                 select(Regulon, value),
               by=c('Regulon')) %>%
-    tibble::column_to_rownames(., var = 'Regulon')
-  rownames(delta_comb[order(rowMeans(abs(delta_comb))),])
+    tibble::column_to_rownames(., var = 'Regulon') %>% 
+    mutate(absDelta=rowMeans(abs(.))) %>%
+    arrange(absDelta)
+  
+  if(return_df){
+    delta_comb
+  } else {
+    rownames(delta_comb)
+  }
 }
 
 # Highly specialized function meant to create pairings of columns by the name 
